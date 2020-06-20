ec97571f4ab533e35bc08facb4d48339c9804bb6
Anthony Richardson <Anthony@anthonyrichardson.com>
2020-06-20 15:26:20 +0930

    Update search filters to match current API

--- lib/saasu/account.rb
+++ lib/saasu/account.rb
@@ -1,4 +1,4 @@
 class Saasu::Account < Saasu::Base
   allowed_methods :show, :index, :destroy, :update, :create
-  filter_by %W(IsActive IsBankAccount AccountType IncludeBuiltIn PageSize Page)
+  filter_by %W(IsActive IsBankAccount AccountType IncludeBuiltIn HeaderAccountId AccountLevel PageSize Page)
 end
--- lib/saasu/contact.rb
+++ lib/saasu/contact.rb
@@ -1,4 +1,4 @@
 class Saasu::Contact < Saasu::Base
   allowed_methods :show, :index, :destroy, :update, :create
-  filter_by %W(LastModifiedFromDate LastModifiedToDate GivenName FamilyName IsActive IsCustomer IsSupplier IsContractor IsPartner Tags TagSelection Email ContactId PageSize Page)
+  filter_by %W(LastModifiedFromDate LastModifiedToDate GivenName FamilyName CompanyName CompanyId IsActive IsCustomer IsSupplier IsContractor IsPartner Tags TagSelection Email ContactId PageSize Page)
 end
